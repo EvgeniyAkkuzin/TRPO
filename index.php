96f1000ffb70ac3411135872d75ce04e290757b7
Your Name <429272-10>
2021-04-03 14:44:35 +0300

    lab3

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+namespace Laba3;
+
+use Akkuzin\MyLog;
+
+require_once "core\EquationInterface.php";
+require_once "core\LogAbstract.php";
+require_once "core\LogInterface.php";
+require_once "Akkuzin\Line.php";
+require_once "Akkuzin\MyLog.php";
+require_once "Akkuzin\Quadratic.php";
+MyLog::log("evgeniy");
+MyLog::write();
